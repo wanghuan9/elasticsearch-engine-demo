f451ba253dcba19a8f11b2fd29c539e2f9785b0c
wanghaun <huan2.wang@aihuishou.com>
2022-05-30 22:16:49 +0800

    elasticsearch-engine fix

--- src/test/java/com/elasticsearch/engine/demo/elasticsearchenginedemo/ElasticsearchEngineDemoApplicationTests.java
+++ src/test/java/com/elasticsearch/engine/demo/elasticsearchenginedemo/ElasticsearchEngineDemoApplicationTests.java
@@ -1,10 +1,8 @@
 package com.elasticsearch.engine.demo.elasticsearchenginedemo;
 
-import com.elasticsearch.engine.elasticsearchengine.EnableEsEngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableEsEngine
 @SpringBootApplication
 public class ElasticsearchEngineDemoApplicationTests {
 
