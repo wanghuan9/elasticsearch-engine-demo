b2d5def7722fab45d2e027870d54da6505c4d6dd
wanghaun <huan2.wang@aihuishou.com>
2022-06-29 19:19:25 +0800

    feature/20220629_fix 增加日志配置项

--- src/test/java/com/elasticsearch/engine/demo/InitDataTest.java
+++ src/test/java/com/elasticsearch/engine/demo/InitDataTest.java
@@ -1,6 +1,11 @@
 package com.elasticsearch.engine.demo;
 
+import com.elasticsearch.engine.base.common.proxy.EsQueryProxy;
+import com.elasticsearch.engine.base.common.queryhandler.EsProxyExecuteHandler;
+import com.elasticsearch.engine.demo.domain.es.repository.PersonEsModelRepository;
+import com.elasticsearch.engine.demo.dto.query.PersonBaseQuery;
 import com.elasticsearch.engine.demo.service.PersonTestDataInitService;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.lang.reflect.Proxy;
+import java.util.List;
 
 
 /**
@@ -28,4 +35,20 @@ public class InitDataTest {
     public void initData() {
         personTestDataInitService.insertList(30);
     }
+
+    public static void main(String[] args) {
+        System.setProperty("jdk.proxy.ProxyGenerator.saveGeneratedFiles", "true");
+        System.out.println("$Proxy0.class全名: " + Proxy.getProxyClass(PersonEsModelRepository.class.getClassLoader(), PersonEsModelRepository.class).toString());
+
+        PersonEsModelRepository repository = (PersonEsModelRepository) Proxy.newProxyInstance(
+                PersonEsModelRepository.class.getClassLoader(),
+                new Class[]{PersonEsModelRepository.class},
+                new EsQueryProxy<PersonEsModelRepository>(PersonEsModelRepository.class, true, new EsProxyExecuteHandler(), true));
+
+        PersonBaseQuery person = new PersonBaseQuery();
+        List<String> itemNoList = Lists.newArrayList("US2022060100001");
+        person.setPersonNoList(itemNoList);
+        repository.queryOne(person);
+    }
+
 }
